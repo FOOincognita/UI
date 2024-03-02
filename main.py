b9dc7a5973a46159d89246bd5c49f75a3201e254
FOOincognita <archer1799@gmail.com>
2024-03-01 18:17:02 -0600

    Progress

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
 import streamlit as st
 #!! PUBLIC persistent storage; accessible by multiple employees
 # Define the layout of the app, including the sidebar and main pages
+
 def main():
     st.sidebar.title('Navigation')
-    tab = st.sidebar.radio("Go to", ('Home', 'Table', 'Analysis', 'Settings', 'Chat'))
+    tab = st.sidebar.radio(('Home', 'Table', 'Analysis', 'Settings', 'Chat'))
 
-    if tab == 'Home':
-        home()
-    elif tab == 'Table':
-        data()
-    elif tab == 'Analysis':
-        analysis()
-    elif tab == 'Settings':
-        settings()
-    elif tab == 'Chat':
-        chat()
+    match tab:
+        case 'Home':
+            home()
+        case 'Table':
+            table()
+        case 'Analysis':
+            analysis()
+        case 'Settings':
+            settings()
+        case 'Chat':
+            chat()
+        case _:
+            raise Exception
+
 
 # Define what will be displayed on each page
-def home():
+def home() -> None:
+    """ Welcome & User Manual Link """
     st.title('Home')
     st.write('Welcome to the home page!')
 
-def analysis():
+def table() -> None:
+    """ This page will show rank table """
     st.title('Table')
-    st.write('This is the analysis page. You can add your analysis tools and visualizations here.')
+    st.write('This is the Table page!')
 
-def data():
+def analysis() -> None:
+    """ This page will show analytics fropm table & additional financial analytics """
     st.title('Analytics')
     st.write('This is the data page. Display your datasets or data-related functionalities here.')
 
-def settings():
+def settings() -> None:
+    """ This is a simple settings page """
     st.title('Settings')
     st.write('This is the settings page. Add your configuration settings or user preferences here.')
     
-def chat():
-    st.title('Settings')
+def chat() -> None:
+    st.title('ChatBot')
     st.write('This is the settings page. Add your configuration settings or user preferences here.')
 
 if __name__ == "__main__":
