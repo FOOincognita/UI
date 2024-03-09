3ca67ef0ce7a10f286f218b36e784704af3627c2
FOOincognita <archer1799@gmail.com>
2024-03-08 19:04:09 -0600

    progess

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import streamlit as st
 """
 # [TODO]: 
     1. Implement subtabs for analysis (bubbles)
-    2. Implement Home chatbot screen using streamlit essentials
-    3. Add dataframe & stage table IO
-    4. Brainstrom financial & consumer data we'd like to display
+    2. Implement Home chatbot screen (using streamlit essentials)
+    3. Add dataframe & prepare table input/output for easy back-end connection
+    4. Brainstrom what kinds of financial & consumer data we'd like to display, & using which graph(s)
         - Find sources for this info
     5. Implement settings
-    
 """
 
 def main():
@@ -31,45 +30,54 @@ def main():
 
 
 # Define what will be displayed on each page
+    #* [ALONSO]
 def home() -> None:
     #TODO: Streamlit Essentials has a chat plugin already
     """ Welcome & Chat """
     st.title('Home')
     st.write('<short & sweet intro>\n\n<Chatbot goes here>')
-    #! Description
-    #! ChatBot/Chatbox 
+        #! "Welcome to Neuro-X1's GPT Bot"
+        #! ChatBot/Chatbox "What are you looking to research today?"
     
-
+    #* [ARCHER]
 def output() -> None:
     # TODO: Need to install streamlit essentails & use dataframe plugin
     """ This page will show rank table """
     st.title('Table')
     st.write('This is the Table page!')
+    #* [ARCHER] 
     #! Full Dataframe with Filter, save, & expand
         #> Names should be clickable; sends user to analysis to take cloer look at medication.
+    
+    #! LATER DOWN THE LINE:
+        #> Clickable option to pull up single/specific analysis page
 
+
+    #* [ALONSO]
 def analysis() -> None:
     """ This page will show analytics fropm table & additional financial analytics """
     st.title('Analytics')
     st.write('This is the data page. Display your datasets or data-related functionalities here.')
     #! GRAPHS
-    #! TAB: Drug research stage (eg stage II clinical trials)
-    #! TAB: Drug financials (simialr drugs financials?)
-    #! TAB: Consumer Data
+    #! TOP 5s; Overview for each
+        #? SUB-TAB: Drug research stage (eg stage II clinical trials)
+        #? SUB-TAB: Drug financials (simialr drugs financials?)
+        #? SUB-TAB: Consumer Data
 
-
+    #* [ARCHER]
 def settings() -> None:
     """ This is a simple settings page """
     st.title('Settings')
     st.write('This is the settings page. Add your configuration settings or user preferences here.')
     #! Light/Dark theme changer 
-    #! Ticket system
+    #! Ticket system; Setup Google Form using @neurox1.com email
     #! Data sync between users 
-    #! User IDs
-    #! Retrain model option
-    #! Find out where it will be ran:
-        #> If local, mutex lock
-        #> If Azure, no problem
+        #> If local, R/W lock
+        #> If Azure, no problem; only 1 person can access anyways
+        
+        
+    #! LATER DOWN THE LINE: TAB: Retrain model?
+    
 
 if __name__ == "__main__":
     main()
